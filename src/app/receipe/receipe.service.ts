d0c56db425681b08f81a789ec51ca2f26848ea57
shrikant patki <shrikant.patki@innovect.com>
2020-05-07 00:09:07 +0530

    [SP] done with android build

--- src/app/receipe/receipe.service.ts
+++ src/app/receipe/receipe.service.ts
@@ -17,6 +17,18 @@ export class ReceipeService {
       title:'Masala Dosa',
       ingrediants:['butter','Dosa','Bhaji'],
       imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Dosa_at_Sri_Ganesha_Restauran%2C_Bangkok_%2844570742744%29.jpg/220px-Dosa_at_Sri_Ganesha_Restauran%2C_Bangkok_%2844570742744%29.jpg'
+    },
+    {
+      id:'r3',
+      title:'Idli',
+      ingrediants:['Idli','sambar','chatany'],
+      imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Idli_-_A_Traditional_Indian_Food.JPG/220px-Idli_-_A_Traditional_Indian_Food.JPG'
+    },
+    {
+      id:'r4',
+      title:'Uttapa',
+      ingrediants:['rice','sambar','chatany'],
+      imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Mini_Uttappam.jpg/250px-Mini_Uttappam.jpg'
     }
   ]
   constructor() { }
